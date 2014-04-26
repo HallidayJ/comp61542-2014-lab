665a4cf756fe27ef5483503e2cc3b1cb7c7d6ae5
HallidayJ <jon.halliday@ctds.eu>
2014-04-26 15:29:26 +0100

    Tidied up test_search after GitHub error

--- test/test_search.py
+++ test/test_search.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from os import path
 import unittest
 
@@ -7,8 +6,8 @@ from comp61542.database import database
 class TestSearch(unittest.TestCase):
 
     def setUp(self):
-        dir, _ = path.split(__file__)
-        self.data_dir = path.join(dir, "..", "data")    
+        directory, _ = path.split(__file__)
+        self.data_dir = path.join(directory, "..", "data")    
     
     def test_get_author_publication_number(self):
         author = "Caroline Jay"
@@ -139,152 +138,3 @@ class TestSearch(unittest.TestCase):
         self.assertTrue(db.read(path.join(self.data_dir, "dblp_seperation_test.xml")))
         data = db.degree_of_separation(author1, author2)
         self.assertEqual(data, ["X"])
-
-  
-if __name__ == '__main__':
-=======
-from os import path
-import unittest
-
-from comp61542.database import database
-
-class TestSearch(unittest.TestCase):
-
-    def setUp(self):
-        dir, _ = path.split(__file__)
-        self.data_dir = path.join(dir, "..", "data")    
-    
-    def test_get_author_publication_number(self):
-        author = "Caroline Jay"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, number = db.get_author_publication_number(author)
-        self.assertEqual(number, 18)
-    
-    def test_get_author_conference_number(self):
-        author = "Caroline Jay"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, number = db.get_author_conference_number(author)
-        self.assertEqual(number, 8)
-    
-    def test_get_author_journal_number(self):
-        author = "Caroline Jay"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, number = db.get_author_journal_number(author)
-        self.assertEqual(number, 9)
-    
-    def test_get_author_bookchapter_number(self):
-        author = "Caroline Jay"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, number = db.get_author_bookchapter_number(author)
-        self.assertEqual(number, 1)
-    
-    def test_get_author_book_number(self):
-        author = "Caroline Jay"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, number = db.get_author_book_number(author)
-        self.assertEqual(number, 0)
-        
-    def test_get_author_coauthor_number(self):
-        author = "Caroline Jay"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, number = db.get_author_coauthor_number(author)
-        self.assertEqual(number, 20)
-    
-    def test_get_author_first_on_paper_number(self):
-        author = "Caroline Jay"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, number = db.get_author_first_on_paper_number(author)
-        self.assertEqual(number, 7)
-        
-    def test_get_author_last_on_paper_number(self):
-        author = "Caroline Jay"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, number = db.get_author_last_on_paper_number(author)
-        self.assertEqual(number, 1)
-    
-    def test_get_author_sole_on_paper_number(self):
-        author = "Stefano Ceri"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, number = db.get_author_sole_on_paper_number(author)
-        self.assertEqual(number, 8)
-        
-    def test_search_by_name_part(self):
-        authorpart = "Car"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, data = db.get_author_details(authorpart)
-        self.assertEqual(len(data), 23)
-    
-    def test_count_get_author_by_publication(self):
-        author = "Caroline Jay"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, data = db.get_author_publications(author)
-        self.assertEqual(data, [[8, 9, 0, 1, 18]])
-
-    def test_count_get_author_first_place_by_publication(self):
-        author = "Caroline Jay"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, data = db.get_author_first_place(author)
-        self.assertEqual(data, [[2, 4, 0, 1, 7]])
-  
-    def test_count_get_author_last_place_by_publication(self):
-        author = "Caroline Jay"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, data = db.get_author_last_place(author)
-        self.assertEqual(data, [[1, 0 ,0 ,0 ,1]])
-  
-    def test_count_get_author_sole_place_by_publication(self):
-        author = "Stefano Ceri"
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_curated_sample.xml")))
-        _, data = db.get_author_sole_place(author)
-        self.assertEqual(data, [[7, 0, 0, 1, 8]])
-        
-    def test_degree_of_seperation_seperationof0(self):
-        author1 = ""
-        author2 = ""
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_seperation_test.xml")))
-        data = db.get_author_seperation(author1, author2)
-        self.assertEqual(data, [0])
-    
-    def test_degree_of_seperation_seperationof1(self):
-        author1 = ""
-        author2 = ""
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_seperation_test.xml")))
-        data = db.get_author_seperation(author1, author2)
-        self.assertEqual(data, [1])
-
-    def test_degree_of_seperation_seperationof2(self):
-        author1 = ""
-        author2 = ""
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_seperation_test.xml")))
-        data = db.get_author_seperation(author1, author2)
-        self.assertEqual(data, [2])
-
-    def test_degree_of_seperation_noconnection(self):
-        author1 = ""
-        author2 = ""
-        db = database.Database()
-        self.assertTrue(db.read(path.join(self.data_dir, "dblp_seperation_test.xml")))
-        data = db.get_author_seperation(author1, author2)
-        self.assertEqual(data, ["X"])
-
-  
-if __name__ == '__main__':
->>>>>>> b883a72dca89c36c184ee94e4998d71e71ecbf13
-    unittest.main()
